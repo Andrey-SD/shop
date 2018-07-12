b1b2b0861282616bb3536309e6d6d282c1783760
Andrey-sd <andrey.sd.post@gmail.com>
2018-07-12 11:37:32 +0300

    checkout for feature_create_mvc_arhitecture

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use App\Init;
-
-require_once dirname(__FILE__) . '/vendor/autoload.php';
-Init::start();
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Init;
+
+//require_once dirname(__FILE__) . '/vendor/autoload.php';
+//Init::start();
+
+echo dirname(__FILE__);
