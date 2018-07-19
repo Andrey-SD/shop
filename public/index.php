bb14c9fbaf7c275972d7240f992a69cb743476d8
Andrey <andrey.sd.post@gmail.com>
2018-07-20 00:45:53 +0300

    progress validation

--- public/index.php
+++ public/index.php
@@ -7,102 +7,101 @@ use Models\Users;
 require_once dirname( __DIR__).'/app/Init.php';
 Init::start();
 //Route::routing();
-$post = ['name' => 'ыыы',
-    'email' => 'ы',
+
+
+
+$request = ['name' => 'dd5_dnd',
+    'email' => 'dddd@gg.dddd',
     'password' => '12345',
     're_password' => '12345'];
 $rules = [
-    'name'=>'required|maximum:64|minimum:6|unique|charnum|injection',
-    'email'=>'required|minimum:6|unique|email|injection',
+    'name'=>'required|maximum:64|minimum:6|unique:name|charnum|injection',
+    'email'=>'required|minimum:6|unique:email|email|injection',
     'password' => 'required',
     're_password' => 'required|repeat:password'
 ];
 
-run($post, $rules);
-    function run($post, $rules){
-        $arg=null;
-        foreach ($rules as $field => $rule) {
+$validation = new Validation;
+$validation->run($request, $rules);
+
+class Validation
+{
+    private $request;
+    
+    function run($request, $rules)
+    {
+        $this->request = $request;
+        $arg = null;
+        foreach ($rules as $field=>$rule){
             $explode = explode('|', $rule);
-            foreach ($explode as $action) {
+            foreach ($explode as $action){
                 $explode_action[1] = null;
                 $explode_action = explode(':', $action);
                 $function = $explode_action[0];
                 if(isset($explode_action[1])){
                     $arg = $explode_action[1];
                 }
-                $result = $function($post[$field],$arg);
+                $result = $this->$function($request[$field],$arg);
                 if($result){
                    echo $field.' '.$result.'<br>';
                 }
             }
         }
     }
-
-
-    function required($value){
+    
+    function required($value)
+    {
         if(empty($value)){
-            return 'Поле не может быть пустым';
+            return 'Введите данные';
+        }
+    }
+    
+    function maximum($value, $arg){
+         if (strlen($value) > $arg){
+             return 'Максимум '.$arg.' символов';
+         }
+    }
+
+    function minimum($value,$arg){
+         if (strlen($value) < $arg){
+             return 'Минимум '.$arg.' символов';
+         }
+    }
+
+    function unique($value, $arg){
+//        $users = new Users;
+//        $result = $users->find([$arg=>$value]);
+//        if($result){
+//           return $value.' уже занято.'; 
+//        }
+        
+    }
+
+    function charnum($value){
+        if(preg_match ('/[^\w]/u',$value)){
+            return 'Только буквы, цифры и _';
         }
     }
 
-     function maximum($value, $arg){
-         if (strlen($value) > $arg){
-             return 'Поле не может быть больше '.$arg.' символов';
-         }
-    }
-
-     function minimum($value,$arg){
-         if (strlen($value) < $arg){
-             return 'Поле не может быть меньше '.$arg.' символов';
-         }
-    }
-
-     function unique($value){
-//        $users = new Users;
-//         $post = ['name' => 'ыыы',
-//             'email' => 'ы',
-//             'password' => '12345',
-//             're_password' => '12345'];
-//        $users->find([$value=>$post[key($value)]]);
-//        var_dump($users);
+    function injection($value){
         
     }
 
-     function charnum($value){
-        
+    function email($value){
+        if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
+            return 'E-mail должен содержать точки и @';
+        }
     }
 
-     function injection($value){
-        
+    function repeat($value, $arg){
+        if ($value != $this->request[$arg]){
+            return 'Поля не совподают';
+        }
     }
 
-     function email($value){
-        
-    }
-
-     function repeat($value, $arg){
-        
-    }
-
-     function error($value, $error){
+    function error($value, $error){
 
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-//$ = $request->validate([
-//    'title' => 'required|unique:posts|max:255',
-//    'body' => 'required',
-//]);
-
+}
 
